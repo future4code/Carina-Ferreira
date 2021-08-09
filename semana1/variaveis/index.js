b695b39c6e70145114fa982e5fce7129ad3021b7
Carina Santos Ferreira <carinasf14@gmail.com>
2021-08-09 17:09:35 -0300

    exercicios-variaveis

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,41 @@
+/* 
+Exercícios interpretação:
+1- A primeira impressão será 10 e a segunda será 10 5
+2- A impressão será 10 10 10
+3-
+let horasTrabalhoDia = prompt("Quantas horas você trabalha por dia?")
+let salarioDiario = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${salarioDiario/horasTrabalhoDia} por hora`) 
+
+Exercícios escrita:
+*/
+
+//let nome
+//let idade 
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+// Os tipos de variaveis foram impressos como indefinidos porque representa a falta de valor da variável
+
+
+let seuNome = prompt("Qual o seu nome?")
+let suaIdade = prompt("Qual a sua idade?")
+
+
+console.log(typeof seuNome, typeof suaIdade)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
