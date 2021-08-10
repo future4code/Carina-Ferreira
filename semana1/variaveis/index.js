f22869172b112e4f5391450a1112e0be6767eed4
Carina Santos Ferreira <carinasf14@gmail.com>
2021-08-09 23:11:15 -0300

    exercicios-variaveis

--- semana1/variaveis/index.js
+++ semana1/variaveis/index.js
@@ -19,23 +19,25 @@ Exercícios escrita:
 // Os tipos de variaveis foram impressos como indefinidos porque representa a falta de valor da variável
 
 
-let seuNome = prompt("Qual o seu nome?")
-let suaIdade = prompt("Qual a sua idade?")
+//let seuNome = prompt("Qual o seu nome?")
+//let suaIdade = prompt("Qual a sua idade?")
+//let seuNome = "Carina"
+//let suaIdade = 23
 
+//console.log(typeof seuNome, typeof suaIdade)
 
-console.log(typeof seuNome, typeof suaIdade)
+// Agora que o valor da variável foi adicionado, o tipo de variável ficou como string por ser texto e number por ser número 
 
+//console.log("Olá, meu nome é", seuNome, "e tenho", suaIdade, "anos")
 
+//Questão 2
 
+//let estaChovendo = false
+//let estaSol = true
+//let estaCalor = false
+//console.log("Está chovendo?", false, "Está sol?", true, "Está calor?", false)
 
-
-
-
-
-
-
-
-
-
-
-
+let a = 10
+let b = 25
+let c = a
+console.log("O valor de a é", a = b, "O valor de b é", b = c) 
