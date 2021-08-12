23844d30077cbc835d5ca001ce447712d9258ec4
Carina Santos Ferreira <carinasf14@gmail.com>
2021-08-12 17:04:47 -0300

    exercicios-funcoes

---
+++ semana1/funcoes/template_javascript/index.js
@@ -0,0 +1,38 @@
+//  Exercícios de interpretação de códigos
+
+// Exercício 1
+// a.
+// 10 e 50
+
+// b.
+// A informação ficaria armazenada, mas nada seria exibido no console.
+
+// Exercício 2
+// a. A função faz com que o texto inserido pelo usuário fique em letra
+// maiúscula e informe se possui ou não o elemento "cenoura", sendo true or false
+
+// b.
+// i. EU GOSTO DE CENOURA true
+// ii. CENOURA É BOM PRA VISTA true
+// iii. CENOURAS CRESCEM NA TERRA true
+
+// Exercícios de escrita de código
+
+// Exercício 1
+//a.
+
+// function imprimeDados(){
+//     const meusDados = ("Eu sou Carina, tenho 23 anos, moro em São Paulo e sou estudante")
+//     console.log(meusDados)
+// }
+
+// imprimeDados() 
+
+//b.
+function imprimeDados(nome, idade, cidade, profissao) {
+    const dados = (`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}`)
+    console.log(dados)
+
+}
+function imprimeDados(Bianca, vintetres, SãoPaulo, nutricionista)
+console.log(dados)
