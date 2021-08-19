888632158802eaf5e6e25b6389e9106911e61da6
Carina Santos Ferreira <carinasf14@gmail.com>
2021-08-18 23:02:23 -0300

    iniciei-exercicios-loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,51 @@
+//Exercícios de interpretação de código
+
+
+
+
+
+
+
+// Exercícios de escrita de código
+
+// Exercício 1
+
+// let usuarioPossuiAnimais = Number (prompt("Quantos animais de estimação você tem?"))
+
+// if (usuarioPossuiAnimais === 0) {
+//     console.log("Que pena! Você pode adotar um pet!")
+// }
+
+// let listaAnimaisUsuario = []
+// let i = 0
+
+// while (i < usuarioPossuiAnimais) {
+//     nomeAnimais = prompt("Qual é o nome dos seus animais de estimação?")
+//     i++
+//     listaAnimaisUsuario.push(nomeAnimais)
+ 
+// }
+
+// console.log("Os nomes dos seus bichinhos de estimação são:", listaAnimaisUsuario)
+
+
+
+
+
+
+// Considere que você tenha acesso a um array  (chamado de 'array original') que é composto somente de números.
+// Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+
+
+
+
+// a) Escreva um programa que **imprime** cada um dos valores do array original.
+
+// b) Escreva um programa que **imprime** cada um dos valores do array original divididos por 10
+
+// c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e **imprima** esse novo array
+
+// d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`". 
+// Depois, **imprima** este novo array.
+
+// e) Escreva um programa que imprima no console o maior e o menor números contidos no array original
