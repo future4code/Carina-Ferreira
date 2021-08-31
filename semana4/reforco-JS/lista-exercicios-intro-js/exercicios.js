3e9246640403bcf1a4ccb4939e3ca3bfd60a0ba1
Carina Santos Ferreira <carinasf14@gmail.com>
2021-08-31 19:11:28 -0300

    finalizei lista de exercicios JS

--- semana4/reforco-JS/lista-exercicios-intro-js/exercicios.js
+++ semana4/reforco-JS/lista-exercicios-intro-js/exercicios.js
@@ -149,9 +149,14 @@ function checaAnoBissexto(ano) {
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
-  const usuarioEhMaiorDeIdade = prompt("Você tem mais de 18 anos?")
-  const usuarioPossuiEnsinoMedio = prompt ("Você possui ensino médio completo?")
-  const usuariopPossuiDisponibilidadeHorario = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
-console.log(usuarioEhMaiorDeIdade === "sim" && usuarioPossuiEnsinoMedio === "sim" && usuariopPossuiDisponibilidadeHorario === "sim")
+  conts usuarioEhMaiorDeIdadeVocê tem mais de 18 anos?
+  Você possui ensino médio completo?
+  Você possui disponibilidade exclusiva durante os horários do curso?
 }
+
+
+
+
+
+
 
