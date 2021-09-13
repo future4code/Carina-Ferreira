1855dc848dad34af13c26d18c9352eed18250849
Carina Santos Ferreira <carinasf14@gmail.com>
2021-09-13 00:08:15 -0300

    Atualizei os exercícios

--- semana5/componentes-classe/insta4/src/App.js
+++ semana5/componentes-classe/insta4/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Post from './components/Post/Post';
+import {IconeComContador} from './components/IconeComContador/IconeComContador';
+import { SecaoComentario } from './components/SecaoComentario/SecaoComentario';
 
 
 const MainContainer = styled.div`
@@ -11,26 +13,84 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+state = {
+
+  posts: [
+    {
+      nomeUsuario: 'paulinha',
+      fotoUsuario: 'https://picsum.photos/50/50?random=0',
+      fotoPost: 'https://picsum.photos/200/150?random=0'
+    },
+
+    {
+      nomeUsuario: 'catarina',
+      fotoUsuario: 'https://picsum.photos/50/50?random=1',
+      fotoPost: 'https://picsum.photos/200/150?random=1'
+    },
+    {
+      nomeUsuario: 'marcela',
+      fotoUsuario: 'https://picsum.photos/50/50?random=2',
+      fotoPost: 'https://picsum.photos/200/150?random=2',
+    }
+  ],
+  valorInputUsuario: '',
+  valorInputFotoUsuario: '',
+  valorInputFotoPost: '',
+}
+
+adicionaPost = () => {
+  const novoPost = {
+    nomeUsuario: this.state.valorInputUsuario,
+    fotoUsuario: this.state.valorInputFotoUsuario,
+    fotoPost: this.state.valorInputFotoPost,
+  }
+  const OutrosPosts = [...this.state.posts, novoPost];
+  this.setState({posts: OutrosPosts, valorInputUsuario: '', valorInputFotoUsuario: '', valorInputFotoPost: ''});
+};
+
+onChangeInputUsuario = (event) => {
+  this.setState({ valorInputUsuario: event.target.value});
+};
+
+onChangeInputFotoUsuario = (event) => {
+  this.setState({ valorInputFotoUsuario: event.target.value});
+};
+
+onChangeInputFotoPost = (event) => {
+  this.setState({valorInputFotoPost: event.target.value});
+};
   render() {
-    return (
+   
+      const listaDePosts = this.state.posts.map((post) => {
+        return (
+          <Post
+            nomeUsuario = {post.nomeUsuario} fotoUsuario = {post.fotoUsuario} fotoPost = {post.fotoPost}
+          />
+        );
+      });
+
+      return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?random=0'}
-          fotoPost={'https://picsum.photos/200/150?random=0'}
+        <input
+        value={this.state.valorInputUsuario}
+        onChange={this.onChangeInputPessoa}
+        placeholder={"Usuário"}
         />
 
-        <Post
-          nomeUsuario={'catarina'}
-          fotoUsuario={'https://picsum.photos/50/50?random=1'}
-          fotoPost={'https://picsum.photos/200/150?random=1'}
+        <input
+        value={this.state.valorInputFotoUsuario}
+        onChange={this.onChangeInputFotoUsuario}
+        placeholder={"Foto"}
         />
 
-        <Post
-          nomeUsuario={'marcela'}
-          fotoUsuario={'https://picsum.photos/50/50?random=2'}
-          fotoPost={'https://picsum.photos/200/150?random=2'}
+        <input
+        value={this.state.valorInputFotoPost}
+        onChange={this.onChangeInputFotoPost}
+        placeholder={"Imagem"}
         />
+        <button onClick={this.props.adicionaPost}>Adicionar</button>
+          {listaDePosts}
       </MainContainer>
     );
   }
