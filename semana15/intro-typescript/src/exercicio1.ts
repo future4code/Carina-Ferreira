93f6bb83b168f3f48768a2b19a0ab41915e610be
Carina Santos Ferreira <carinasf14@gmail.com>
2021-11-17 21:47:25 -0300

    Exercicios de introdução ao typescript

---
+++ semana15/intro-typescript/src/exercicio1.ts
@@ -0,0 +1,8 @@
+// Exercício 1
+// a) Crie uma variável minhaString do tipo string e atribua um valor a ela. Tente atribuir um número a esta variável. O que acontece?
+
+// let minhaString: string = "Oi, sou uma string"
+// minhaString = "Sou uma nova string"
+// minhaString = 3
+
+// Resposta: Como a variável foi tipada como string, não é possível declarar ela depois como número, pois quebra o código.
