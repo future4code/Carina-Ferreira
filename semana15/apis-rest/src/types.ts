590bcbcafa608ba38b92d6b2e868d607b81e005a
Carina Santos Ferreira <carinasf14@gmail.com>
2021-12-02 03:24:18 -0300

    Finalizei os exercícios

---
+++ semana15/apis-rest/src/types.ts
@@ -0,0 +1,14 @@
+export type user = {
+    id: number;
+    name: string;
+    email: string;
+    type: UserType;
+    age: number;
+  };
+
+export enum UserType {
+ADMIN = "ADMIN",
+NORMAL = "NORMAL"
+}
+
+  
